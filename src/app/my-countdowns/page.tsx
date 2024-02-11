fbaca1cb56508b93fa0d7546de7a195d005b0b6a
psegarra <pausegarra.b@gmail.com>
2024-02-11 12:21:50 +0100

    🍺 get user countdowns

--- src/app/my-countdowns/page.tsx
+++ src/app/my-countdowns/page.tsx
@@ -1,12 +1,18 @@
+import { getSession } from '@/helpers/get-session';
+import exec from '@/lib/database';
 import Link from 'next/link';
 
-export default function Page() {
+export default async function Page() {
+  const session = await getSession();
+  const countdowns: any = await exec({ query: 'select * from countdowns where user_id = ?', values: [(session?.user as any).id] })
+
   return (
     <div className="container mx-auto mt-4">
       <div className="flex justify-between">
         <h1 className='text-4xl'>My Countdowns</h1>
         <Link href="/my-countdowns/add" className='btn btn-primary'><span className='text-2xl font-bold'>+</span> Add Countdown</Link>
       </div>
+      {countdowns.map((countdown: any) => <p>{countdown.name}</p>)}
     </div>
   );
 }
